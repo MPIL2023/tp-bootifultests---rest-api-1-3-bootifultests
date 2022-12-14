e0f6d5d371a435ae06e69e5f093759f9bcf679d7
Nada-ach <achahbounenada@gmail.com>
2022-12-14 11:46:33 +0100

    RetAssured test getCurrent

--- src/test/java/com/example/demo/BootifultestsApplicationTests.java
+++ src/test/java/com/example/demo/BootifultestsApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.demo;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BootifultestsApplicationTests {
+	private final static String BASE_URI = "http://localhost:8090";
+	@LocalServerPort
+	private int port;
+
+	@Before
+	public void configureRestAssured() {
+		RestAssured.baseURI = BASE_URI;
+		RestAssured.port = port;
+	}
 
 	@Test
-	void contextLoads() {
+	public void getCurrent() {
+		given()
+		.baseUri("http://localhost:8090")
+				.when()
+				.get("/adder/current")
+				.then()
+				.log().all()
+				.assertThat().statusCode(200);
+
 	}
 
 }
